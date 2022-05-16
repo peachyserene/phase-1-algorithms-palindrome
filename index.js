f6eac113de95302ff6a0e98c7ab9e5d15c1f6b6d
Georgia Burnside <burnsidegeorgia@gmail.com>
2022-05-16 13:11:18 -0400

    commit tree e10d20c98c46d8ccad096f0e6038deeaba6b5ee7

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  let wordArray = word.split("");
+
+  let y = 0;
+  let reversedWord = wordArray
+    .map((letter) => {
+      y--;
+      let letterB = wordArray[wordArray.length + y];
+      return letterB;
+    })
+    .join("");
+
+  console.log(reversedWord === word ? true : false);
+  return reversedWord === word ? true : false;
 }
 
 /* 
@@ -7,7 +19,11 @@ function isPalindrome(word) {
 */
 
 /*
-  Add written explanation of your solution here
+  put string into an array.
+  divide string in half. if num of letters is odd, 1st half rounds down, 2nd half rounds down.
+  send 1st half to one array and the 2nd to another array. 
+  reverse the 2nd array.
+if 1st array is === to 2nd array return true, else return false
 */
 
 // You can run `node index.js` to view these console logs
